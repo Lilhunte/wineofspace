84171dc1fa6989e5e850219b57f2771dfc7b5cd9
Lilhunte <122054779+Lilhunte@users.noreply.github.com>
2023-06-19 15:58:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+
+show= alert("Calisto Anselmo made this page, please rate us for better experience.")
+security= prompt(`This web page is protected:
+please enter password to navegate`)
+
+if (security !== "calisto"){
+    window.close()
+}
+else if (security === null){
+    window.close()
+}
+else{
+    wellc= alert("WELCOME to WINE OF SPACE")
+}
